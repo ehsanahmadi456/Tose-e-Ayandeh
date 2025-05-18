5434d1fd34d11dc7454fa229953b06985c8eceeb
Ehsan <ehsanahmadi8338@gmail.com>
2025-05-18 19:29:10 +0330

    design all page and all sections

---
+++ script.js
@@ -0,0 +1,9 @@
+var module_43688894253 =
+  ($(document).ready(function () {
+    var hc = $(".circlesBig .contCircle .circle").height();
+    $(".circlesBig .contCircle .circle").width(hc);
+  }),
+  void $(window).resize(function () {
+    var hc = $(".circlesBig .contCircle .circle").height();
+    $(".circlesBig .contCircle .circle").width(hc);
+  }));
